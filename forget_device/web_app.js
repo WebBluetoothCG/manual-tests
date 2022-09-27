833b3da1ce97d72a9e3d0c44ad8f421c8175ba29
gsinafirooz <sinafirooz@google.com>
2022-09-27 13:53:55 -0700

    Add manual test for forget API

--- forget_device/web_app.js
+++ forget_device/web_app.js
@@ -14,7 +14,7 @@
  *  limitations under the License.
  */
 
- "use strict";
+"use strict";
 
 // Espruino devices publish a UART service by default.
 const nordicUARTService = '6e400001-b5a3-f393-e0a9-e50e24dcca9e';
