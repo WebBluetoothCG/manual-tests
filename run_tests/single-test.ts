b6b2da1b6d459d668205dafb5ac973e165d2bf5e
evelyn masso <coding@outofambit.com>
2023-09-19 15:44:53 -0700

    Update single-test.ts

--- run_tests/single-test.ts
+++ run_tests/single-test.ts
@@ -1,15 +1,16 @@
 import t from "tap";
 
 export const runSingleTest = (name: string) => {
-  t.before(() => {
-    console.log("individual test setup goes here");
-  });
   t.test(name, (t) => {
+    t.before(() => {
+      console.log("individual test setup goes here");
+    });
+    t.after(() => {
+      console.log("individual test teardown goes here");
+    });
+
     t.pass("this passes");
     t.fail("this fails");
     t.end();
-  });
-  t.after(() => {
-    console.log("individual test teardown goes here");
   });
 };
