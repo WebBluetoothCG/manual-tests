83137aac1d9e4770bc1de854618bd1774ea777d4
evelyn masso <coding@outofambit.com>
2023-09-19 10:04:12 -0700

    list example folders

--- run_tests/index.ts
+++ run_tests/index.ts
@@ -1,1 +1,24 @@
-console.log("hello");
+import * as fs from "fs";
+import * as path from "path";
+
+const pathFromDirent = (dir: fs.Dirent): string =>
+  path.join(dir.path, dir.name);
+
+const run = () => {
+  const directoryPath = path.parse(__dirname).dir;
+  const exampleFolders = fs
+    .readdirSync(directoryPath, {
+      withFileTypes: true,
+    })
+    .filter((f) => {
+      return (
+        f.isDirectory() &&
+        fs.readdirSync(pathFromDirent(f)).some((file) => {
+          return file === "index.html";
+        })
+      );
+    });
+  console.log(exampleFolders);
+};
+
+run();
