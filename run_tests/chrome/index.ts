8f5dafe9546bc44402c605b895b7a0568f483459
evelyn masso <coding@outofambit.com>
2023-09-21 15:53:21 -0700

    chrome: open espruino page

--- run_tests/chrome/index.ts
+++ run_tests/chrome/index.ts
@@ -3,7 +3,7 @@ import { BrowserDriver, BrowserNames } from "../driver";
 import assert from "assert";
 
 let browser: Browser | null;
-let page: Page | null;
+let mainPage: Page | null;
 
 const assertBrowser = (browser: Browser | null): Browser => {
   assert(browser, "Browser not launched in time");
@@ -20,12 +20,25 @@ export const chromeDriver: BrowserDriver = {
     browser = await puppeteer.launch();
   },
   createSession: async (pageUrl: string) => {
-    page = await assertBrowser(browser).newPage();
-    await page.goto(pageUrl);
+    mainPage = await assertBrowser(browser).newPage();
+    await mainPage.goto(pageUrl);
   },
-  uploadDeviceCode: () => {},
+  uploadDeviceCode: async () => {
+    // open espruino tab
+    await assertPage(mainPage).locator("#btn_load_code").click();
+    const target = await assertBrowser(browser).waitForTarget(
+      (t) => t.url().match(/espruino\.com/) !== null,
+    );
+    const espruinoPage = await target?.page();
+    if (!espruinoPage) {
+      throw "couldn't find loaded espruino page";
+    }
+    await espruinoPage.bringToFront();
+    // bluetooth stuff
+  },
   endSession: async () => {
-    await assertPage(page).close();
+    const openPages = await assertBrowser(browser).pages();
+    await openPages.map((p) => p.close());
   },
   shutdown: async () => {
     await assertBrowser(browser).close();
